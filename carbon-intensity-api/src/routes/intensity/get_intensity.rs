498a8fe486931fd01fef59a93b2826f2128f88b4
Andrea Di Lisio <andrea.dilisio91@gmail.com>
2025-01-04 18:19:00 +0100

    cleanup

--- carbon-intensity-api/src/routes/intensity/get_intensity.rs
+++ carbon-intensity-api/src/routes/intensity/get_intensity.rs
@@ -2,9 +2,7 @@ use actix_web::{HttpRequest, HttpResponse, Responder};
 
 use super::entities::{CarbonIntensityData, Intensity, IntensityIndex};
 
-pub async fn get_intensity(req: HttpRequest) -> std::io::Result<impl Responder> {
-    println!("{:?}", req.headers());
-
+pub async fn get_intensity(_: HttpRequest) -> std::io::Result<impl Responder> {
     // return just mock data for now
     let data = CarbonIntensityData {
         from: "2018-01-20T12:00Z".to_string(),
